d155adf4da81203b421ac225ca299f72f8891368
angrun <angrun@ttu.ee>
2019-04-15 21:30:11 +0300

    security fix

--- src/main/java/com/app/appbackend/config/WebSecurityConfig.java
+++ src/main/java/com/app/appbackend/config/WebSecurityConfig.java
@@ -1,9 +1,20 @@
 package com.app.appbackend.config;
 
+import com.app.appbackend.security.JwtAuthenticationFilter;
+import com.app.appbackend.security.JwtAuthorizationFilter;
+import com.app.appbackend.user.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,23 +24,24 @@ import java.util.Arrays;
 import static com.app.appbackend.utils.Utils.ALLOWED_ADDRESS;
 
 @Configuration
-/*@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)*/
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-/*
+
     @Autowired
-    private UserService userService;
+    private UserServiceImpl userService;
+
     @SuppressWarnings("deprecation")
     @Bean
     public static NoOpPasswordEncoder passwordEncoder() {
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    }*/
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
                 .csrf().disable()
-                /*.authorizeRequests()
+                .authorizeRequests()
                 .antMatchers("/", "/*.png", "/*.jpg", "/*.jpeg", "/register", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -38,25 +50,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .addFilter(new JwtAuthorizationFilter(authenticationManager()))
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)*/;
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    /* @Override
-     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
-     }
-     @Bean(BeanIds.AUTHENTICATION_MANAGER)
-     @Override
-     public AuthenticationManager authenticationManagerBean() throws Exception {
-         return super.authenticationManagerBean();
-     }
- */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(ALLOWED_ADDRESS));
-        /*configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(Arrays.asList("Authorization"));*/
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(Arrays.asList("Authorization"));
         configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control",
                 "Content-Type", "Authorization"));
